247bc90cddf9542ea59cf2449b9236540064ab36
mashiken <tatsumikogorou@gmail.com>
2020-04-26 21:45:08 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '80f2c8901442ec90a0c6014e690788d35b7aea3d1dcb826ca3b761beaebf3569360f2f769ef28387f69becc5832278edde58d86b527dca58b135f1a2a10d8a05'
